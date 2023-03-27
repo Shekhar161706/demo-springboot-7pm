717b99f3c65cd847a0274d7c3877f245bdf1ae65
RockAmbika <123267967+Shekhar161706@users.noreply.github.com>
2023-03-28 00:39:38 +0530

    Assignment calculator..

---
+++ src/main/java/com/example/demospringboot7pm/controller/controller/Calculator.java
@@ -0,0 +1,37 @@
+package com.example.demospringboot7pm.controller.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Calculator {
+
+    @RequestMapping("/add")
+    public int add() {
+        int a = 3;
+        int b = 4;
+        int sum = a + b;
+        return sum;
+    }
+    @RequestMapping("/sub")
+    public  int subtract(){
+        int c = 5;
+        int d = 4;
+        int sub = c - d;
+        return sub;
+    }
+    @RequestMapping("/multi")
+    public  int multiply() {
+        int a = 4;
+        int b = 4;
+        int multi = a * b;
+        return multi;
+    }
+    @RequestMapping("/devide")
+    public  double devide() {
+        int a = 12;
+        int b = 5;
+        double div = a / b;
+        return div;
+    }
+}
