e2ff2b22e616645ebeb521e1259fbc117e18af39
RockAmbika <123267967+Shekhar161706@users.noreply.github.com>
2023-03-28 00:50:32 +0530

    Assignment calculator..

---
+++ src/main/java/com/example/demospringboot7pm/controller/controller/DemoSpringboot7PmApplication.java
@@ -0,0 +1,9 @@
+package com.example.demospringboot7pm.controller.controller;
+
+import org.springframework.boot.SpringApplication;
+
+public class DemoSpringboot7PmApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringboot7PmApplication.class, args);
+    }
+    }
